50b28b99d0cf1d47a7819c3fa7ea0292f74fa22d
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-06 02:08:52 +0530

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second, result = 0;
+            Console.Write("Enter the first number :");
+            first = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number :");
+            second = int.Parse(Console.ReadLine());
+            /*
+             *  first type 1
+             *  
+                        try
+                        {
+
+                        result = first / second;
+                        Console.WriteLine(result);
+                        }
+                        catch (DivideByZeroException )
+                        {
+                            Console.WriteLine(o);
+
+                        }
+            */
+            /* 
+             *  second type 2
+              try
+             {
+
+                 result = first / second;
+                 Console.WriteLine(result);
+             }
+             catch (DivideByZeroException o)
+             {
+                 Console.WriteLine(o.Message);
+
+             }*/
+
+
+            try
+            {
+
+                result = first / second;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException o)
+            {
+                Console.WriteLine("hello");
+
+            }
+        }
+    }
+}
